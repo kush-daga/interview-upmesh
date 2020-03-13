39385d50d6892fd356342f0738981d7389bd5468
Kush Daga <kushdaga@hotmail.com>
2020-03-13 10:46:18 +0530

    client

---
+++ client/src/components/Divider.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React, { Component } from "react";
+
+export default class Divider extends Component {
+	render() {
+		return <div>divider</div>;
+	}
+}
---
+++ client/src/components/Hero.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React, { Component } from "react";
+
+export default class Hero extends Component {
+	render() {
+		return <div>hero</div>;
+	}
+}
